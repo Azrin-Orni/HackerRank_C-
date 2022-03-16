f2527f2fb0a1a98d6800ee52cd3be7ffc47e514e
Azrin Hossain <azrinhossain16@gmail.com>
2022-03-16 11:34:32 +0600

    Create Input_Output.cpp

---
+++ Input_Output.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main() {
+
+    int sum = 0;
+    int x, y, z;
+    cin >> x >> y >> z;
+    sum = x + y + z;
+    cout << sum << endl;
+
+    return 0;
+}
